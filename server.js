2993aeb79f90bd9dd3bb2570b4f92c4a4dc76d43
Shashank <shashank.kaphle@gmail.com>
2022-12-15 16:56:07 +0545

    CHanged cors position

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
-app.use(cors())
 app.use(express.json())
+app.use(cors())
 app.use(express.urlencoded({extended:false}))
 app.use(errorHandler)
 
