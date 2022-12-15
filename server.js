6190c525cf24cbe084bfea0e7ee365224d4792d9
Shashank <shashank.kaphle@gmail.com>
2022-12-15 17:01:22 +0545

    Hello route added

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({extended:false}))
 app.use(errorHandler)
-
+app.get('/',(req,res)=>{
+    res.send('Hello')
+})
 
 app.use('/api/users',require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
